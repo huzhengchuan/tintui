f13c29fd0d7c4106007f50429d080f34753c017a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-22 05:43:37 +0000

    Deploy tiny0918/tintui to github.com/tiny0918/tintui.git:gh-pages

---
+++ precache-manifest.886c32d53a9d0b198907ca6742dc47e7.js
@@ -0,0 +1,67 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80c2e7f73c2c5ada31ebdcd6ea228d8",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main~24120820.8f46756af3c71060ff40.bundle.js"
+  },
+  {
+    "url": "runtime~main.8f46756af3c71060ff40.bundle.js"
+  },
+  {
+    "revision": "dd7b3f3915a4b6b84d46",
+    "url": "static/css/main~24120820.d9949bab.chunk.css"
+  },
+  {
+    "url": "vendors~main~0ad7406a.8f46756af3c71060ff40.bundle.js"
+  },
+  {
+    "url": "vendors~main~0ad7406a.8f46756af3c71060ff40.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main~0decbd73.8f46756af3c71060ff40.bundle.js"
+  },
+  {
+    "url": "vendors~main~0decbd73.8f46756af3c71060ff40.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main~0f485567.8f46756af3c71060ff40.bundle.js"
+  },
+  {
+    "url": "vendors~main~1f20a385.8f46756af3c71060ff40.bundle.js"
+  },
+  {
+    "url": "vendors~main~1f20a385.8f46756af3c71060ff40.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main~2a42e354.8f46756af3c71060ff40.bundle.js"
+  },
+  {
+    "url": "vendors~main~678f84af.8f46756af3c71060ff40.bundle.js"
+  },
+  {
+    "url": "vendors~main~678f84af.8f46756af3c71060ff40.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main~67b22fc5.8f46756af3c71060ff40.bundle.js"
+  },
+  {
+    "url": "vendors~main~b58f7129.8f46756af3c71060ff40.bundle.js"
+  },
+  {
+    "url": "vendors~main~b58f7129.8f46756af3c71060ff40.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main~d939e436.8f46756af3c71060ff40.bundle.js"
+  },
+  {
+    "url": "vendors~main~d939e436.8f46756af3c71060ff40.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main~ec8c427e.8f46756af3c71060ff40.bundle.js"
+  },
+  {
+    "url": "vendors~main~ec8c427e.8f46756af3c71060ff40.bundle.js.LICENSE.txt"
+  }
+]);
